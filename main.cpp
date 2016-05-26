07e0cc31a635ed976133868806147b1b98f3c39c
hadoop73 <python35@163.com>
2016-05-26 20:51:02 +0800

    空间构造、析构和添加型别完成 100%

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ using namespace std;
 int main() {
     for (int i = 28; i < 1000; ++i) {
         cout << i << "  hello world " << (i%128)* sizeof(int) << endl;
-        auto p = MySTL::alloc::allocate( (i%128)* sizeof(int)  );
-        //auto p = MySTL::allocator<int>::allocate();
-        //MySTL::allocator<int>::construct(p,i);
-        //MySTL::allocator<int>::destroy(p);
-        //MySTL::allocator<int>::deallocate(p);
+        //auto p = MySTL::alloc::allocate( (i%128)* sizeof(int)  );
+        auto p = MySTL::allocator<int>::allocate();
+        MySTL::allocator<int>::construct(p,i);
+        MySTL::allocator<int>::destroy(p);
+        MySTL::allocator<int>::deallocate(p);
     }
     //system("pause");
     return 0;
